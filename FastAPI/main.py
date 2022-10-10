d844e33b620594299cd2e100b15645138ab17743
Your Name <you@example.com>
2022-10-10 19:28:40 +0300

    depl

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,7 +1,7 @@
-import os
+from fastapi import FastAPI
 
-import uvicorn
-from todo.app import app
+app = FastAPI()
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
+@app.get('/')
+def demo():
+    return {"status": "Working"}
